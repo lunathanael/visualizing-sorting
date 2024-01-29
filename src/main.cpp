1c868a3df36bb23e6306de02ea00f49f9c30a654
Nathanael Lu <lunathanael@gmail.com>
2024-01-29 16:41:16 -0500

    might have merge issues

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,28 @@
 #include <cstddef>
 #include <iostream>
 #include <numeric>
+#include <vector>
 
 #include <SFML/Graphics.hpp>
 #include <src/sorting.hpp>
 #include <random>
 
-constexpr float screen_width = 800;
-constexpr float screen_height = 600;
+constexpr int screen_width = 800;
+constexpr int screen_height = 600;
 
 class Sorter : public sf::Drawable {
 private:
-    std::array<int, 500> m_data;
+    std::vector<int> m_data;
     std::mt19937 gen;
-    vis::QuickSort<decltype(m_data)::iterator> m_sorter;
+    vis::QuickSort<std::vector<int>::iterator> *q;
 
 public:
-    Sorter() : m_sorter(m_data.begin(), m_data.end())
+    Sorter()
     {
-        std::iota(m_data.begin(), m_data.end(), 1);
+        m_data.resize(500);
+        std::iota(m_data.begin(), m_data.end(), 0);
         std::shuffle(m_data.begin(), m_data.end(), gen);
+        q = new vis::QuickSort{m_data.begin(), m_data.end()};
     }
 
     inline bool next() {return q->next() == m_data.end(); };
@@ -28,8 +31,8 @@ public:
     void draw(sf::RenderTarget &target, sf::RenderStates states) const override {
         // origin = bottom left
         for (std::size_t i = 0; i < m_data.size(); ++i) {
-            float width = screen_width / m_data.size();
-            float height = (screen_height / m_data.size()) * m_data[i];
+            int width = screen_width / m_data.size();
+            int height = (screen_height / m_data.size()) * m_data[i];
 
             sf::RectangleShape shape(sf::Vector2f(width, height));
             shape.setPosition(i * width, screen_height - height);
@@ -39,21 +42,14 @@ public:
 };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "Sorting", sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "Sorting");
     window.setFramerateLimit(60);
 
     Sorter sorter;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        sorter.next();
-
+    bool progress = false;
+    do
+    {
+        progress = sorter.next();
         window.clear();
         window.draw(sorter);
         window.display();
