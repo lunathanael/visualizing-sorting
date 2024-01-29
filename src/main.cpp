3e92470d6909aa8de516b67a693b29259c137c80
Paul Zupan <paul.zupan.13@gmail.com>
2024-01-29 10:33:15 -0800

    Add SFML integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
 int main() {
-    std::cout << "Hello, Nathanael!\n";
+    sf::Window window(sf::VideoMode(800, 600), "Sorting");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+
     return 0;
 }
