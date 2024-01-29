fec8d31ffcc5cf119f7a254be1a82cb1991abf65
Nathanael Lu <lunathanael@gmail.com>
2024-01-29 14:53:01 -0500

    wip quicksort

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <random>
 
 #include <SFML/Graphics.hpp>
+#include <src/sorting.hpp>
 
 
 int main() {
-    sf::Window window(sf::VideoMode(800, 600), "Sorting");
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-    }
+    constexpr int n = 8;
+    std::vector<int> v(n);
     
+    std::mt19937 rng;
+
+    std::iota(v.begin(), v.end(), 0);
+    std::shuffle(v.begin(), v.end(), rng);
+
+    vis::BogoSort bs{v.begin(), v.end(), rng};
+
+    bool ended = false;
+    do
+    {
+        ended = bs.next();
+        for(auto & a : v)
+        {
+            std::cout << a << ' ';
+        }
+        std::cout << '\n';
+    }
+    while(!ended);
     return 0;
 }
--- src/sorting.hpp
+++ src/sorting.hpp
@@ -3,6 +3,7 @@
 
 #include <random>
 #include <algorithm>
+#include <iterator>
 
 namespace vis
 {
@@ -14,7 +15,7 @@ namespace vis
         T itr1;
         T itr2;
 
-    private:
+    public:
         QuickSort(const T begin, const T end)
             : itr1(begin)
             , itr2(end)
@@ -31,7 +32,7 @@ namespace vis
         T itr2;
         RNG rng;
 
-    private:
+    public:
         BogoSort(const T begin, const T end, const RNG rng)
             : itr1(begin)
             , itr2(end)
@@ -47,8 +48,53 @@ namespace vis
 template <typename T>
 bool vis::QuickSort<T>::next()
 {
-    return false;   
+    if(itr1 >= itr2)
+    {
+        return true;
+    }
+
+    T pivot = itr1, i = first, j = first;
+    std::advance(j, 1);
+    while (j < itr2)
+    {
+        if((*j) < (*pivot))
+        {
+            std::advance(i, 1);
+            std::iter_swap(i, j);
+            ++j;
+            return false;
+        }
+        ++j;
+    }
+    if(i == pivot)
+    {
+        return false;
+    }
+    std::iter_swap(i, pivot);
 }
+
+void quick_sort(int* first, int* last, CMP cmp) {
+	if (first < last) {
+		int pivot = *first;
+		int* i = first; int* j = first + 1;
+		while (j < last) {
+			if (cmp(*j, pivot)) {
+				++i;
+				int temp = *i;
+				*i = *j;
+				*j = temp;
+			}
+			++j;
+		}
+		int temp = *i;
+		*i = *first;
+		*first = temp;
+		quick_sort(first, i, cmp);
+		quick_sort(i + 1, last, cmp);
+	}
+	return;
+}
+
 
 template <typename T, typename RNG>
 bool vis::BogoSort<T, RNG>::next()
