110c65b116df287f34dc7da54c1b72c6c2372c0a
lunathanael <lunathanael@gmail.com>
2024-02-01 13:40:26 -0500

    fixed bogosort

--- include/vis/backend/bogo_sorter.hpp
+++ include/vis/backend/bogo_sorter.hpp
@@ -16,7 +16,8 @@ namespace vis::backend {
         BogoSort(const T begin, const T end, const RNG rng)
             : itr1(begin)
             , itr2(end)
-            , rng(rng) {}
+            , rng(rng) 
+        {}
 
         T begin() const override { return itr1; }
 
@@ -24,7 +25,7 @@ namespace vis::backend {
 
         bool is_done() const override { return std::is_sorted(itr1, itr2); }
 
-        void next() override { std::shuffle(itr1, itr2); }
+        T next() override { std::shuffle(itr1, itr2, rng); return itr2; }
     };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,12 @@
 #include <SFML/Graphics.hpp>
 
 #include "vis/backend/quick_sorter.hpp"
+#include "vis/backend/bogo_sorter.hpp"
 #include "vis/frontend/sorter.hpp"
 
-using Container = std::array<int, 200>;
+using Container = std::array<int, 6>;
 using Iterator = Container::iterator;
-using Sorter = vis::backend::QuickSort<Iterator>;
+using Sorter = vis::backend::BogoSort<Iterator, std::mt19937>;
 
 Container create_data() {
     Container data;
@@ -30,7 +31,7 @@ int main() {
     window.setFramerateLimit(60);
 
     Container numbers = create_data();
-    std::unique_ptr<Sorter> backend = std::make_unique<Sorter>(numbers.begin(), numbers.end());
+    std::unique_ptr<Sorter> backend = std::make_unique<Sorter>(numbers.begin(), numbers.end(), std::mt19937());
     vis::frontend::Sorter<Iterator> frontend(std::move(backend));
 
     while (window.isOpen()) {
